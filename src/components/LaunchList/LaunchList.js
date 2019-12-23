25083e177f79038eaa6929b909dfcc1c1c1822aa
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-23 12:21:41 -0700

    Implemented SAVE_FAVORITES and added ability to put a favorite launch into redux store.

--- src/components/LaunchList/LaunchList.js
+++ src/components/LaunchList/LaunchList.js
@@ -3,9 +3,11 @@ import LaunchCard from '../../containers/LaunchCard/LaunchCard.js';
 import { connect } from 'react-redux';
 import './LaunchList.css';
 
-export const LaunchList = ({ launches }) => {
+export const LaunchList = ({ launches, saveFavorite }) => {
   const launchesData = launches.map((launch, index) => {
-    return <LaunchCard launch={launch} key={index} />;
+    return (
+      <LaunchCard launch={launch} saveFavorite={saveFavorite} key={index} />
+    );
   });
   return <div className='grid-3'>{launchesData}</div>;
 };
--- src/containers/LaunchCard/LaunchCard.js
+++ src/containers/LaunchCard/LaunchCard.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './LaunchCard.css';
 
-export const LaunchCard = ({ launch }) => {
+export const LaunchCard = ({ launch, saveFavorite }) => {
   return (
     <div className='launch-card'>
       <p>Flight Number: {launch.flight_number}</p>
       <p>Mission: {launch.mission_name}</p>
       <p>Launch: {launch.launch_year}</p>
       <p>Site: {launch.launch_site.site_name_long}</p>
-      <button className='save-button'>SAVE</button>
+      <button onClick={() => saveFavorite(launch)} className='save-button'>
+        SAVE
+      </button>
     </div>
   );
 };
 
-export default connect(null, null)(LaunchCard);
+export const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+export default connect(mapStateToProps, null)(LaunchCard);
