e9f29477deb647e171833ed8129c6eae530cae5d
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2020-01-10 11:23:25 -0700

    Rendered specific mission patch for each launch.

--- src/components/LaunchPage/LaunchPage.js
+++ src/components/LaunchPage/LaunchPage.js
@@ -4,11 +4,11 @@ import './LaunchPage.css';
 export const LaunchPage = launch => {
   console.log('launch', launch);
   console.log('launch.rocket', launch.rocket);
-  // console.log('launch.rocket.rocket_id', launch.rocket.rocket_id);
+  console.log('launch.rocket.rocket_id', launch.rocket.rocket_id);
 
   return (
     <div className='launch-page'>
-      {/* <img src={launch.links.mission_patch} alt='Mission Patch' /> */}
+      <img src={launch.links.mission_patch} alt='Mission Patch' />
       <h1>{launch.flight_number}</h1>
     </div>
   );
