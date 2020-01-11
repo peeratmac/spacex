528ba9ad67fe9503965d5a46888416c446f54bae
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2020-01-10 18:38:26 -0700

    Added Wikipedia link for each launch page to corresponding mission

--- src/components/LaunchPage/LaunchPage.js
+++ src/components/LaunchPage/LaunchPage.js
@@ -8,8 +8,15 @@ export const LaunchPage = launch => {
 
   return (
     <div className='launch-page'>
-      <img src={launch.links.mission_patch} alt='Mission Patch' />
-      <h1>{launch.flight_number}</h1>
+      <h1>Mission: {launch.mission_name}</h1>
+      <p>Flight Number: {launch.flight_number}</p>
+      <p>Launch: {launch.launch_year}</p>
+      <p className='site-name'>Site: {launch.launch_site.site_name_long}</p>
+      <img src={launch.links.mission_patch_small} alt='Mission Patch' />
+      <p>
+        More Information:{'   '}
+        <a href={launch.links.wikipedia}>{launch.mission_name}</a>
+      </p>
     </div>
   );
 };
