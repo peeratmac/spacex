3e3d77e2905f4e379efb8ada0061231f2463adbb
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2020-01-13 14:26:27 -0700

    Passed reducer tests for Favorites.

--- src/reducers/favoritesReducer.test.js
+++ src/reducers/favoritesReducer.test.js
@@ -2,6 +2,33 @@ import { favoritesReducer } from './favoritesReducer';
 
 describe('favoritesReducer', () => {
   it('should return the initial state', () => {
-    const expected = '';
+    const expected = [];
+    const result = favoritesReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the correct state with favorites if the action is SAVE_FAVORITES', () => {
+    const initialState = [];
+    const action = {
+      type: 'SAVE_FAVORITES',
+      favorites: [
+        {
+          flight_number: 88,
+          mission_name: 'Crew Dragon In Flight Abort Test'
+        }
+      ]
+    };
+    const result = favoritesReducer(initialState, action);
+    const expectedState = [
+      [
+        {
+          flight_number: 88,
+          mission_name: 'Crew Dragon In Flight Abort Test'
+        }
+      ]
+    ];
+
+    expect(result).toEqual(expectedState);
   });
 });
